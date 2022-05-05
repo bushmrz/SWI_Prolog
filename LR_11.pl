1a1c3382b0b1d42deb474d2ea1f048ef5da50c71
bushmrz <56377675+bushmrz@users.noreply.github.com>
2022-05-05 23:38:27 +0300

    Update LR_11.pl

--- LR_11.pl
+++ LR_11.pl
@@ -69,4 +69,12 @@ mult(X):- X>0,mult_digit(X,1),!.
 mult_digit(0,1):-!.
 mult_digit(X,Y):- X1 is X div 10, P is X mod 10, mult_digit(X1,Y1), Y is Y1*P.
 
+%16
+mult(X,Y):- X>0,!,mult_digit(X,Y,1),!.
+mult_digit(X,Y,Z):- X > 0,!,
+    X1 is X div 10, Z1 is Z*(X mod 10),
+    mult_digit(X1,Y,Z1).
+mult_digit(_,Y,Z):- Y is Z.
+
+
 
