bcd302bd986e192777308dee9185e4c8e9526440
bushmrz <56377675+bushmrz@users.noreply.github.com>
2022-05-13 23:18:05 +0300

    rec  down

--- LR_12.pl
+++ LR_12.pl
@@ -14,3 +14,11 @@ sumSDiv(0,_,0):-!.
 sumSDiv(X,Y,S):- Y mod X =:= 0 -> X1 is X-1,
     sumSDiv(X1,Y,S1),(prime(X) -> S is S1+X; S is S1);
     Y mod X =\=0 -> X1 is X-1, sumSDiv(X1,Y,S1), S is S1.
+
+%down
+sumSDiv_d(X,Y):-X>0,!,sumSDiv_d(X,X,Y,0),!.
+sumSDiv_d(X,Y,S,Z):- X>0,!,
+    (   Y mod X  =:= 0, prime(X) -> Z1 is Z+X, X1 is X-1,
+        sumSDiv_d(X1,Y,S,Z1);
+    Z1 is Z, X1 is X-1, sumSDiv_d(X1,Y,S,Z1)).
+sumSDiv_d(_,_,S,Z):- S is Z.
