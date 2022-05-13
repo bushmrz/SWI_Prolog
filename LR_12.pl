bc6ab4fd9a9987712eb11f54756b259fdf73f4cd
bushmrz <56377675+bushmrz@users.noreply.github.com>
2022-05-13 23:41:42 +0300

    2

--- LR_12.pl
+++ LR_12.pl
@@ -22,3 +22,18 @@ sumSDiv_d(X,Y,S,Z):- X>0,!,
         sumSDiv_d(X1,Y,S,Z1);
     Z1 is Z, X1 is X-1, sumSDiv_d(X1,Y,S,Z1)).
 sumSDiv_d(_,_,S,Z):- S is Z.
+
+%2
+% Найти произведение таких делителей числа,
+%сумма цифр которых меньше, чем сумма цифр исходного числа.
+
+sumDig(X,S):-sumDig(X,S,0),!.
+sumDig(X,Y,S):- X>0,!, Z is X mod 10, S1 is S+Z, X1 is X div 10,
+    sumDig(X1,Y,S1).
+sumDig(_,Y,S):- Y is S.
+
+multDiv(X,Y):- sumDig(X,H), X1 is H, multDiv(X,X,Y,X1),!.
+multDiv(1,_,1,_):-!.
+multDiv(X,Y,S,SD):- Y mod X =:= 0 -> X1 is X-1,
+    multDiv(X1,Y,S1,SD),sumDig(X,H),(H<SD -> S is S1*X; S is S1);
+    Y mod X =\=0 -> X1 is X-1, multDiv(X1,Y,S1,SD), S is S1.
